2da339ddec1164e704adc96755e0d2441f22cb61
Wasantha Herath <wasantha_h@epic.lk>
2016-03-16 16:01:23 +0530

    adding spring security login sample

---
+++ springlogin/src/main/java/com/epic/springlogin/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.epic.springlogin.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
